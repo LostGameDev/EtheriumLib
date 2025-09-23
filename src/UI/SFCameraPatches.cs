b41fb4a88214a8b4a67b94f35faf96ebc708b59e
GameWorld <ehrenf@outlook.com>
2025-09-23 22:42:15 +1000

    Added ScaleformGFxUtils and added SFCameraPatches

---
+++ src/UI/SFCameraPatches.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using Scaleform;
+
+namespace EtheriumLib.UI
+{
+    [HarmonyPatch(typeof(SFCamera))]
+    public static class SFCameraPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("CreateMovieCreationParams", new[] { typeof(string), typeof(int) })]
+        public static void SFCamera_CreateMovieCreationParams_Postfix(ref SFMovieCreationParams __result, ref string swfName)
+        {
+            if (ScaleformGFxUtils.TryGetOverride(swfName, out var overridePath))
+            {
+                __result.MovieName = overridePath;
+                Plugin.Logger.LogInfo($"[SFCamera] Redirected SWF {swfName} -> {overridePath}");
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("CreateMovieCreationParams", new[] { typeof(string), typeof(int), typeof(UnityEngine.Color32), typeof(bool) })]
+        public static void SFCamera_CreateMovieCreationParams_Extended_Postfix(ref SFMovieCreationParams __result, ref string swfName)
+        {
+            if (ScaleformGFxUtils.TryGetOverride(swfName, out var overridePath))
+            {
+                __result.MovieName = overridePath;
+                Plugin.Logger.LogInfo($"[SFCamera] Redirected SWF {swfName} -> {overridePath}");
+            }
+        }
+    }
+}
---
+++ src/UI/ScaleformGFxUtils.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using EtheriumLib.Debug;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace EtheriumLib.UI
+{
+    public static class ScaleformGFxUtils
+    {
+        private static readonly Dictionary<string, string> overrides = new(StringComparer.OrdinalIgnoreCase);
+
+        public static void RegisterOverride(BaseUnityPlugin callerPlugin, string swfName, string assetsPath)
+        {
+            string modId = CallerUtils.GetPluginGUID(callerPlugin);
+
+            string fullPath = Path.Combine(assetsPath, swfName);
+
+            if (File.Exists(fullPath))
+            {
+                overrides[swfName] = fullPath;
+                Plugin.Logger.LogInfo($"[ScaleformGFxUtils] Registered override for {modId}: {swfName} -> {fullPath}");
+            }
+            else
+            {
+                Plugin.Logger.LogWarning($"[ScaleformGFxUtils] Tried to register override but file not found: {fullPath}");
+            }
+        }
+
+        public static bool TryGetOverride(string swfName, out string overridePath)
+        {
+            return overrides.TryGetValue(swfName, out overridePath);
+        }
+    }
+}
